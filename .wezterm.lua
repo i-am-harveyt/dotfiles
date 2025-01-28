f9472a25f51f6b87d779d21dc6b1abfa271346f8
HarveyTung <haotingting30@gmail.com>
2025-01-28 19:23:26 +0800

    Update wezterm font config

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,10 +3,10 @@ local config = wezterm.config_builder()
 
 -- font
 config.font = wezterm.font({
-	family = "IosevkaTerm Nerd Font Mono",
+	family = "ShureTechMono Nerd Font",
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 })
-config.font_size = 18
+config.font_size = 20
 
 -- tab bar
 config.use_fancy_tab_bar = false
@@ -14,5 +14,6 @@ config.tab_bar_at_bottom = true
 
 -- coloerscheme
 config.color_scheme = "Kanagawa (Gogh)"
+
 
 return config
