b0626532ba9e41c0b07bab2e49d540fd2182a3c2
i-am-harveyt <haotingtong30@gmail.com>
2025-02-26 10:45:25 +0800

    Replace: nvim-cmp -> blink.cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,79 +1,15 @@
-local config = function()
-	local cmp = require("cmp")
-	local ls = require("luasnip")
-	local lspkind = require("lspkind")
-
-	cmp.setup({
-		experimental = { ghost_text = { hl_group = "Comment" } },
-		formatting = {
-			format = lspkind.cmp_format({
-				mode = "symbol_text",
-				menu = {
-					buffer = "",
-					nvim_lsp = "",
-					luasnip = "",
-					nvim_lua = "",
-					latex_symbols = "",
-				},
-			}),
-		},
-		mapping = cmp.mapping.preset.insert({
-			["<C-u>"] = cmp.mapping.scroll_docs(-1),
-			["<C-d>"] = cmp.mapping.scroll_docs(1),
-			["<C-CR>"] = cmp.mapping.complete(),
-			["<C-k>"] = cmp.mapping.select_prev_item(),
-			["<C-j>"] = cmp.mapping.select_next_item(),
-			["<C-e>"] = cmp.mapping.abort(),
-			["<CR>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					if ls.expandable() then
-						ls.expand()
-					else
-						cmp.confirm({ select = true })
-					end
-				else
-					fallback()
-				end
-			end),
-
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if ls.locally_jumpable(1) then
-					ls.jump(1)
-				else
-					fallback()
-				end
-			end, { "i" }),
-
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if ls.locally_jumpable(-1) then
-					ls.jump(-1)
-				else
-					fallback()
-				end
-			end, { "i" }),
-		}),
-		snippet = {
-			expand = function(args)
-				ls.lsp_expand(args.body)
-			end,
-		},
-		sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "buffer" } }),
-		window = {
-			completion = cmp.config.window.bordered(),
-			documentation = cmp.config.window.bordered(),
-		},
-		view = {
-			entries = { name = "custom", selection_order = "near_cursor" },
-		},
-	})
-end
-
 return {
 	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "onsails/lspkind.nvim" },
-		config = config,
+		"saghen/blink.cmp",
+		dependencies = "rafamadriz/friendly-snippets",
+		version = "*",
+		opts = {
+			keymap = { preset = "enter" },
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			signature = { enabled = true },
+		},
 	},
-	{ "L3MON4D3/LuaSnip", version = "v2.*" },
 }
