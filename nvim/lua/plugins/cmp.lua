52db5c618e238936b33ab86933b09dab003212f5
i-am-harveyt <haotingtong30@gmail.com>
2025-02-26 12:28:23 +0800

    Complete: migration, keymap setup

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,7 +4,21 @@ return {
 		dependencies = "rafamadriz/friendly-snippets",
 		version = "*",
 		opts = {
-			keymap = { preset = "enter" },
+			keymap = {
+				preset = "default",
+				["<CR>"] = { "accept", "fallback" },
+				["<Tab>"] = { "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
+				["<C-j>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+				["<C-u>"] = { "scroll_documentation_up", "fallback" },
+				["<C-d>"] = { "scroll_documentation_down", "fallback" },
+			},
+			cmdline = {
+				keymap = {
+					["<CR>"] = { "accept_and_enter", "fallback" },
+				},
+			},
 			appearance = {
 				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "mono",
