5cff9ddfaf555785c7ec1ce4d4cee77838063128
i-am-harveyt <haotingtong30@gmail.com>
2025-02-27 14:39:23 +0800

    Tidy the blink.cmp config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,6 +4,16 @@ return {
 		dependencies = "rafamadriz/friendly-snippets",
 		version = "*",
 		opts = {
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			cmdline = {
+				keymap = {
+					preset = "default",
+					["<CR>"] = { "accept_and_enter", "fallback" },
+				},
+			},
 			keymap = {
 				preset = "default",
 				["<CR>"] = { "accept", "fallback" },
@@ -14,17 +24,19 @@ return {
 				["<C-u>"] = { "scroll_documentation_up", "fallback" },
 				["<C-d>"] = { "scroll_documentation_down", "fallback" },
 			},
-			cmdline = {
-				keymap = {
-					preset = "default",
-					["<CR>"] = { "accept_and_enter", "fallback" },
+			signature = { enabled = true },
+			sources = {
+				-- add lazydev to your completion providers
+				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						-- make lazydev completions top priority (see `:h blink.cmp`)
+						score_offset = 100,
+					},
 				},
 			},
-			appearance = {
-				use_nvim_cmp_as_default = true,
-				nerd_font_variant = "mono",
-			},
-			signature = { enabled = true },
 		},
 	},
 }
