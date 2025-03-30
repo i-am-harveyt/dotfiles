d62ce89157b9f45769d7760ac8ee947c8ec9024a
i-am-harveyt <haotingtong30@gmail.com>
2025-03-30 12:52:22 +0800

    Modify: font params management

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,19 +1,49 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- font
-config.font = wezterm.font({
-	family = "ShureTechMono Nerd Font",
-	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-})
-config.font_size = 20
+-- local
+local font = {
+	-- font_family = "DepartureMono Nerd Font Propo",
+	-- font_family = "FantasqueSansM Nerd Font Propo",
+	font_family = "ComicShannsMono Nerd Font Propo",
+	font_size = 22,
+}
 
--- tab bar
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
+config = {
 
--- coloerscheme
-config.color_scheme = "Kanagawa (Gogh)"
+	-- font
+	font = wezterm.font({
+		family = font.font_family,
+		harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	}),
+	font_size = font.font_size,
+	font_rules = {
+		{
+			intensity = "Bold",
+			italic = false,
+			font = wezterm.font(font.font_family, { weight = "Bold", stretch = "Normal", style = "Normal" }),
+		},
+		{
+			intensity = "Bold",
+			italic = true,
+			font = wezterm.font(font.font_family, { weight = "Bold", stretch = "Normal", style = "Italic" }),
+		},
+	},
 
+	-- tab bar
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+	enable_tab_bar = false,
+
+	-- window
+	window_decorations = "RESIZE", -- remove title bar
+
+	max_fps = 120,
+
+	-- color_scheme options
+	-- color_scheme = "Tokyo Night Storm",
+	-- color_scheme = "Gruvbox dark, hard (base16)"
+	color_scheme = "Kanagawa (Gogh)",
+}
 
 return config
