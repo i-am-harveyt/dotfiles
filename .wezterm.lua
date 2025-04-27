1df7afc48dca8a556e849ca52e2cb5e40f0f24e3
i-am-harveyt <haotingtong30@gmail.com>
2025-04-27 22:56:28 +0800

    Modify: font variant config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,43 +1,61 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
 
 -- local
-local font = {
-	-- font_family = "DepartureMono Nerd Font Propo",
-	-- font_family = "FantasqueSansM Nerd Font Propo",
-	font_family = "ComicShannsMono Nerd Font Propo",
-	font_size = 20,
+local fonts = {
+	-- latin_font_family = "ComicShannsMono Nerd Font Propo",
+	-- latin_font_family = "DepartureMono Nerd Font Propo",
+	-- latin_font_family = "FantasqueSansM Nerd Font Propo",
+	-- latin_font_family = "IosevkaTerm Nerd Font Propo",
+	-- latin_font_family = "JetBrainsMono Nerd Font Propo",
+	-- latin_font_family = "GoMono Nerd Font Propo",
+	-- latin_font_family = "UbuntuMono Nerd Font Propo",
+	latin_font_family = "ZedMono Nerd Font Propo",
+	zh_font_family = "DFKai-SB",
+	-- zh_font_family = "Chenyuluoyan",
+	font_size = 22,
 }
 
-config = {
-
-	-- font
-	font = wezterm.font({
-		family = font.font_family,
-		harfbuzz_features = {
-			"calt=0",
-			"clig=0",
-			"liga=0",
+return {
+	font = wezterm.font_with_fallback({
+		{
+			family = fonts.latin_font_family,
+			harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+		},
+		{
+			family = fonts.zh_font_family,
+			harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 		},
 	}),
-	font_size = font.font_size,
+	font_size = fonts.font_size,
 	font_rules = {
 		{
 			intensity = "Bold",
 			italic = false,
-			font = wezterm.font(font.font_family, {
-				weight = "Bold",
-				stretch = "Normal",
-				style = "Normal",
+			font = wezterm.font_with_fallback({
+				{
+					family = fonts.latin_font_family,
+					weight = "Bold",
+				},
+				{
+					family = fonts.zh_font_family,
+					weight = "Bold",
+				},
 			}),
 		},
 		{
-			intensity = "Bold",
+			intensity = "Half",
 			italic = true,
-			font = wezterm.font(font.font_family, {
-				weight = "Bold",
-				stretch = "Normal",
-				style = "Italic",
+			font = wezterm.font_with_fallback({
+				{
+					family = fonts.latin_font_family,
+					weight = "Thin",
+					italic = true,
+				},
+				{
+					family = fonts.zh_font_family,
+					weight = "Thin",
+					italic = true,
+				},
 			}),
 		},
 	},
@@ -53,9 +71,8 @@ config = {
 	max_fps = 120,
 
 	-- color_scheme options
-	color_scheme = "Tokyo Night Storm",
+	-- color_scheme = "Tokyo Night Storm",
 	-- color_scheme = "Gruvbox dark, hard (base16)"
 	-- color_scheme = "Kanagawa (Gogh)",
+	color_scheme = "rose-pine",
 }
-
-return config
